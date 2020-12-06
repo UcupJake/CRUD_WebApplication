61e77498ceb6caa6f5a6c92a15cfed6e18ebc545
YusufJake <yusufjohan05@gmail.com>
2020-12-06 21:38:39 +0700

    CRUD Web Application sudah siap

---
+++ Default.aspx
@@ -0,0 +1,44 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="CRUD_WebApplication._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <br />
+    <div style="background-color: #6699FF; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #FFFFFF; text-align: center; font-size: 25px;">
+        <asp:Label ID="Label3" runat="server" Text="Data Registrasi Pengguna"></asp:Label>
+    </div>
+    <br />
+    <br />
+    <br />
+    <asp:Label ID="Label4" runat="server" Text="ID"></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="TextBox3" runat="server" Width="210px"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label1" runat="server" Text="Nama"></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="TextBox1" runat="server" Width="210px" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+    <br />
+    <asp:Label ID="Label2" runat="server" Text="Email"></asp:Label>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:TextBox ID="TextBox2" runat="server" Width="210px"></asp:TextBox>
+    <br />
+    <br />
+    <div style="text-align: center">
+        <asp:Button ID="Button1" runat="server" Text="Masuk" OnClick="Button1_Click" />
+        <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Cari" />
+        <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Membaharui" />
+        <asp:Button ID="Button4" runat="server" Text="Hapus" OnClick="Button4_Click" OnClientClick="return confirm('Apa anda yakin ingin menghapus email ini?')" />
+    </div>
+    
+
+    <br />
+    <br />
+    <div style="text-align:center">
+        <asp:Label ID="lblmsg" runat="server" Text="Info Masuk" Font-Bold="True"></asp:Label>
+    </div>
+
+    <br />
+    <br />
+    <asp:GridView ID="GridView1" runat="server">
+    </asp:GridView>
+
+</asp:Content>
---
+++ Default.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using CRUD_WebApplication.ServiceReference1;
+
+namespace CRUD_WebApplication
+{
+    public partial class _Default : Page
+    {
+        ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            InsertUser insertUser = new InsertUser();
+            insertUser.Name = TextBox1.Text;
+            insertUser.Email = TextBox2.Text;
+            string r = client.Insert(insertUser);
+            lblmsg.Text = r.ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            ServiceReference1.gettestdata gettestdata = new ServiceReference1.gettestdata();
+            gettestdata = client.GetInfo();
+            DataTable dataTable = new DataTable();
+            dataTable = gettestdata.usertab;
+            GridView1.DataSource = dataTable;
+            GridView1.DataBind();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            UpdateUser updateUser = new UpdateUser();
+            updateUser.UID = int.Parse(TextBox3.Text);
+            updateUser.Name = TextBox1.Text;
+            updateUser.Email = TextBox2.Text;
+            string result = client.Update(updateUser);
+            lblmsg.Text = result.ToString();
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            DeleteUser deleteUser = new DeleteUser();
+            deleteUser.UID = int.Parse(TextBox3.Text);
+            string res = client.Delete(deleteUser);
+            lblmsg.Text = res.ToString();
+        }
+    }
+}
